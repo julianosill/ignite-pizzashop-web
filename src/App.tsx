ce55349c0234c8853a0741a568084ee3d6361868
Juliano Sill <sill.juliano@gmail.com>
2024-01-14 23:14:33 -0300

    build(eslint): install and setup config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { Button } from './components/ui/button'
 import './global.css'
 
+import { Button } from './components/ui/button'
+
 export function App() {
-  return <div className='flex justify-center items-center h-screen'>
-    <Button>Enviar</Button>
-  </div>
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button>Enviar</Button>
+    </div>
+  )
 }
