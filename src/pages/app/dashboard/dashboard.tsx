22b6c97fade03739fa8324e44569e20fbd35cd37
Juliano Sill <sill.juliano@gmail.com>
2024-01-16 14:49:45 -0300

    feat(Dashboard): create page and number cards

---
+++ src/pages/app/dashboard/dashboard.tsx
@@ -0,0 +1,25 @@
+import { Helmet } from 'react-helmet-async'
+
+import { DayOrdersCard } from './day-orders-card copy'
+import { MonthCanceledOrdersCard } from './month-canceled-orders-card'
+import { MonthOrdersCard } from './month-orders-card'
+import { MonthRevenueCard } from './month-revenue-card'
+
+export function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+
+        <div className="grid grid-cols-2 gap-4 lg:grid-cols-4">
+          <MonthRevenueCard />
+          <MonthOrdersCard />
+          <DayOrdersCard />
+          <MonthCanceledOrdersCard />
+        </div>
+      </div>
+    </>
+  )
+}
